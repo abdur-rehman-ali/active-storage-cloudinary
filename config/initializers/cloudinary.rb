3b9932aa61931c714ab9d5e6fad2288c088191c1
abdur-rehman-ali <alijoyia911@gmail.com>
2022-08-23 20:56:07 +0500

    Resolve production issues

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+    config.cloud_name = 'cloudstorage101'
+    config.api_key = '376597832291554'
+    config.api_secret = 'mbEOrp22UCM8Xxjk2ZtQa7C86v4'
+    config.secure = true
+  end
